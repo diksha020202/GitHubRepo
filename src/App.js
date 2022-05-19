ecc8a12abf0dae9a8e0d749935ae264a493f7e67
Diksha Sharma <diksdelhi0202@gmail.com>
2022-05-19 12:25:31 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+import './App.css';
+import Github from './components/GitHub';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Github
+      
